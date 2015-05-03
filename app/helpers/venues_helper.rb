b9c65326423d2b4415a6f3ba134c29d744abf5c0
Staphany Park <stapark008@gmail.com>
2015-05-03 05:30:26 -0400

    New icons.

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,21 +1,18 @@
 module VenuesHelper
   def venue_icon_url(venue)
-    image_url("#{venue.sensors['icon']}_48x.png")
     people = venue.sensors['people']
     water = venue.sensors['hasWater']
     food = venue.sensors['hasFood']
     temp = venue.sensors['hasHeat']
     power = venue.sensors['hasPower']
     if water
-      image_url('water_48x.png')
+      image_url('water_pin.png')
     elsif food
-      image_url('food_48x.png')
+      image_url('food_pin.png')
     elsif temp
-      image_url('heat_48x.png')
+      image_url('heat_pin.png')
     elsif power
-      image_url('power_48x.png')
-    else
-      image_url('trouble_48x.png')
+      image_url('power_pin.png')
     end
   end
 end
